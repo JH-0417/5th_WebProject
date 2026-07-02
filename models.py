6df5d2042359d9bceb3cbfa8fafe6ffab66b90bf
JH-0417 <rjg6844@gmail.com>
2026-07-02 17:02:57 +0900

    스키마 및 컬럼 수정

--- models.py
+++ models.py
@@ -17,15 +17,15 @@ class MemberDB(Base):
     public_id       = Column(String, unique=True, default=generate_uuid, nullable=False)        # 공개 id(웹에서 사용)
     login_id        = Column(String(12), unique=True, nullable=False)                           # 로그인 id
     hashed_password = Column(String, nullable=False)                                            # pw 암호화
-    name            = Column(String, nullable=False)                                            # 이름
-    student_id      = Column(String, unique=True, nullable=False)                               # 학번
-    department      = Column(String, nullable=True)                                             # 과
-    phone           = Column(String, nullable=True)                                             # 폰 번호
-    role            = Column(String, default="member")                                          # 역할 admin, pm, member
-    is_approved     = Column(Boolean, default=False)                                            # 승인 여부
-    github          = Column(String, nullable=True)                                             # GitHub 주소
+    name            = Column(String(30), nullable=False)                                        # 이름
+    student_id      = Column(String(10), unique=True, nullable=False)                           # 학번
+    department      = Column(String(50), nullable=True)                                         # 과
+    role            = Column(String(10), default="member", nullable=False)                      # 역할 admin, pm, member
+    is_approved     = Column(Boolean, default=False)                                            # 가입 승인 여부
+    github          = Column(String(39), nullable=True)                                         # GitHub name
     bio             = Column(Text, nullable=True)                                               # 소개
-    tech_stack       = Column(String, nullable=True)                                            # 아래 참조
+    tech_stack      = Column(Text, nullable=True)                                               # 아래 참조
+    
     """ ex)
         언어         Python, Java, C++, JavaScript, TypeScript, Kotlin, Swift
         프론트       React, Vue, Next.js, Flutter
